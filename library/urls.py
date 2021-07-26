77d9934fcbeba3068c7526768095ab3c90bb9ce6
Pepo <email@pepe.ru>
2021-07-26 15:14:53 +0300

    Добавляет публичную часть сайта с отображением авторов и их книг

--- library/urls.py
+++ library/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from . import views
+from .views import AuthorListView
 
 urlpatterns = [
-    
+    path('', AuthorListView.as_view())
 ]
--- library/views.py
+++ library/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import author, book
+from django.http import HttpResponse
+from django.views import generic
 # Create your views here.
+
+class AuthorListView(generic.ListView):
+    model = author
