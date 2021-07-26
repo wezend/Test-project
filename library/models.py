72b59ac36e808cef11966f9a43ccb98d9c841044
Pepo <email@pepe.ru>
2021-07-26 15:04:12 +0300

    Добавляет сущности автора и книги + добавляет library в settings, добавляет файл urls  и соединяет его с серверным urls

--- library/models.py
+++ library/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField
 
 # Create your models here.
+
+class author(models.Model):
+    name = CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+class book(models.Model):
+    author = models.ForeignKey(
+        author,
+        on_delete=CASCADE
+    )
+    title = CharField(max_length=15)
+
+    def __str__(self):
+        return self.title
+        
